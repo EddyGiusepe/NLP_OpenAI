93fb4bb0c08133a9ee6dd6b820875b9d6d577826
EddyGiusepe <eddychirinos.unac@gmail.com>
2023-01-29 14:54:04 -0300

    Pré-processamento concluído

--- 4_Building_Mini_chatGPT/MiniChatGPT_DistilGPT-2.py
+++ 4_Building_Mini_chatGPT/MiniChatGPT_DistilGPT-2.py
@@ -14,9 +14,31 @@ social. Quanto mais dados estiverem disponíveis, melhor o modelo entenderá e g
 import pandas as pd
 
 # Load data into a pandas dataframe
-data = pd.read_csv("your_text_data.file")
+data = pd.read_csv("train.csv")
+print(data.shape)
+print(data.columns)
+print('')
+print(data.head(7))
 
-# Preprocessing the data
-data = data.dropna()  # remove missing values
-data = data.drop_duplicates()  # remove duplicate values
-data = data.sample(frac=1)  # shuffle the data
+
+# Eliminando as colunas que não precissamos 
+data.drop(['question_id', 'question', 'document_title', 'label'], axis=1, inplace=True)
+# Renomeando o neme da coluna 'answer' para 'text'
+data.rename(columns={'answer': 'text'}, inplace=True)
+print(data.head(7))
+
+# Pré-processamento dos Dados 
+data = data.dropna()  # Remove valores missing
+data = data.drop_duplicates()  # Remove valores duplicados 
+data = data.sample(frac=1)  # Shuffle (embaralhar) os dados 
+print(data.shape)
+print(data.head(8))
+
+'''
+Step 2: Tokenization
+
+Depois que os dados forem coletados e pré-processados, a próxima etapa é tokenizar o texto.
+A tokenização é o processo de dividir o texto em palavras ou subpalavras individuais.
+Isso pode ser feito usando uma biblioteca como tokenizers NLTK ou Hugging Face.
+'''
+
